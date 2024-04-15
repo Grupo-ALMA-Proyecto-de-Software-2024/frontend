92f06b801494ef539e15df8a39132b2792cd875a
Franzo1 <franzwiderstrom1@outlook.com>
2024-04-15 16:54:42 -0400

    feat: Add dropdowns for filter system

--- src/app/data_page/DataSearcher.tsx
+++ src/app/data_page/DataSearcher.tsx
@@ -1,80 +1,142 @@
 "use client";
-import { useState } from 'react';
+import React, { useState, FC } from 'react';
+import {
+    Stack,
+    OutlinedInput,
+    InputLabel,
+    MenuItem,
+    Chip,
+    Select,
+    FormControl,
+    Container
+} from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check"
+import CancelIcon from "@mui/icons-material/Cancel"
 import styles from "./data.module.css";
-import ImageSearcher from './TypesSearcher/ImagesSearcher';
-import VADPSearcher from './TypesSearcher/VADPSearcher';
-import MeasurementSetSearcher from './TypesSearcher/MeasurementSetSearcher';
-import SevenGroupSearcher from './MoleculeSearcher/SevenGroupSearcher';
-import SixGroupSearcher from './MoleculeSearcher/SixGroupSearcher';
-import FiveGroupSearcher from './MoleculeSearcher/SevenGroupSearcher';
+
+interface ElementsInSelect {
+    title: string; 
+    values: string[];
+}
+
+let regiones: ElementsInSelect = {
+    title: "Regiones",
+    values: [
+        "Ophiucus",
+        "Lupus",
+        "Upper Scorpius"
+    ]
+}
+
+let discos: ElementsInSelect = {
+    title: "Discos",
+    values: [
+        "Disco 1",
+        "Disco 2",
+        "Disco 3",
+        "Disco 4",
+        "Disco 5",
+        "Disco N"
+    ]
+}
+
+let bandas: ElementsInSelect = {
+    title: "Bandas",
+    values: [
+        "Banda 6",
+        "Banda 7",
+        "Banda 8",
+        "Banda N"
+    ]
+}
+
+let datos: ElementsInSelect = {
+    title: "Datos",
+    values: [
+        "Contínuo",
+        "Molécula 1",
+        "Molécula 2",
+        "Molécula 3",
+        "Molécula 4",
+        "Molécula N",
+    ]
+}
+
+let archivos: ElementsInSelect = {
+    title: "Archivos",
+    values: [
+        "Measurement Set",
+        "Mapa",
+        "Cubo de canales",
+        "Momento 0",
+        "Momento 1"
+    ]
+}
+
+const MultiSelect: FC<ElementsInSelect> = ({ title, values }) => {
+    const [selectedNames, setSelectedNames] = useState([]);
+    return (
+        <FormControl sx={{ m: 4, width: 500 }}>
+            <InputLabel>{title}</InputLabel>
+            <Select
+                multiple
+                value={selectedNames}
+                onChange={(e) => setSelectedNames(e.target.value)}
+                input={<OutlinedInput label="Multiple Select" />}
+                renderValue={(selected) => (
+                    <Stack gap={1} direction="row" flexWrap="wrap">
+                        {selected.map((value) => (
+                            <Chip
+                                key={value}
+                                label={value}
+                                onDelete={() =>
+                                    setSelectedNames(
+                                        selectedNames.filter((item) => item !== value)
+                                    )
+                                }
+                                deleteIcon={
+                                    <CancelIcon
+                                        onMouseDown={(event) => event.stopPropagation()}
+                                    />
+                                }
+                            />
+                        ))}
+                    </Stack>
+                )}
+            >
+                {values.map((value) => (
+                    <MenuItem
+                        key={value}
+                        value={value}
+                        sx={{ 
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        {value}
+                        {selectedNames.includes(value) ? <CheckIcon color="info" /> : null}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    );
+};
 
 const DataSearcher = () => {
-
-    const [showImage, setShowImage] = useState(false);
-    const [showVADP, setShowVADP] = useState(false);
-    const [showMeasurement, setShowMeasurement] = useState(false);
-
-    const setOnlyImage = () => {
-        setShowImage(true)
-        setShowVADP(false)
-        setShowMeasurement(false)
-    }
-    
-    const setOnlyVADP = () => {
-        setShowImage(false)
-        setShowVADP(true)
-        setShowMeasurement(false)
-    }
-    
-    const setOnlyMeasurement = () => {
-        setShowImage(false)
-        setShowVADP(false)
-        setShowMeasurement(true)
-    }
-
-    const [showSeven, setShowSeven] = useState(false)
-    const [showSix, setShowSix] = useState(false)
-    const [showFive, setShowFive] = useState(false)
     
     return (
-        <div>
-            <form className={styles.dataSearcher}>
-                <h4>Select category: </h4>
-                <div className={styles.dataSearcherRadio}>
-                    <span><input type="radio" id="AS" name="category" value="AS_209"></input></span>
-                    <label htmlFor="AS">AS_209</label>
-                    <span><input type="radio" id="GM" name="category" value="GM_Aur"></input></span>
-                    <label htmlFor="GM">GM_Aur</label>
-                    <span><input type="radio" id="HD" name="category" value="HD_163296"></input></span>
-                    <label htmlFor="HD">HD_163296</label>
-                    <span><input type="radio" id="IM" name="category" value="IM_Lup"></input></span>
-                    <label htmlFor="IM">IM_Lup</label>
-                    <span><input type="radio" id="MWC" name="category" value="MWC_480"></input></span>
-                    <label htmlFor="MWC">MWC_480</label>
-                    <span><input type="radio" id="aux" name="category" value="Aux"></input></span>
-                    <label htmlFor="aux">Aux</label>
-                </div>
-            </form>
-            <form className={styles.dataSearcher}>
-                <h4>Select type of data: </h4>
-                <div className={styles.dataSearcherRadio}>
-                    <span><input type="radio" onChange={()=>setOnlyImage()} id="images" name="category" value="Images"></input></span>
-                    <label htmlFor="images">Images</label>
-                    <span><input type="radio" onChange={()=>setOnlyVADP()} id="vadp" name="category" value="VADP"></input></span>
-                    <label htmlFor="vadp">VADP</label>
-                    <span><input type="radio" onChange={()=>setOnlyMeasurement()} id="measurement" name="category" value="Measurement Sets"></input></span>
-                    <label htmlFor="measurement">Measurement Sets</label>
-                </div>
-            </form>
-            { showImage ? <div><ImageSearcher showSeven={showSeven} showSix={showSix} showFive={showFive} 
-                            setShowSeven={setShowSeven} setShowSix={setShowSix} setShowFive={setShowFive}/> 
-                            { showSeven ? <SevenGroupSearcher /> : null} 
-                            { showSix ? <SixGroupSearcher /> : null} 
-                            { showFive ? <FiveGroupSearcher /> : null} 
-                            </div> : null }
-            { showVADP ? <VADPSearcher /> : null }
-            { showMeasurement ? <MeasurementSetSearcher /> : null }
-        </div>
+        <Container
+            sx={{ 
+                border: 1,
+                borderRadius: '16px',
+                justifyContent: "center",
+            }}
+        >
+            <MultiSelect title={regiones.title} values={regiones.values} />
+            <MultiSelect title={discos.title} values={discos.values} />
+            <MultiSelect title={bandas.title} values={bandas.values} />
+            <MultiSelect title={datos.title} values={datos.values} />
+            <MultiSelect title={archivos.title} values={archivos.values} />
+        </Container>
     )
 }
 
