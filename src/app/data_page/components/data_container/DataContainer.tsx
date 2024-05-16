2e164529820cb9f605beb98dfd0cd3f29b5a2724
Bastián Vera B <bastian.vera@ug.uchile.cl>
2024-05-16 13:01:32 -0400

    Revert "Merge branch 'data_page_dataContainer' of https://github.com/Grupo-ALMA-Proyecto-de-Software-2024/frontend into data_page_dataContainer"
    This reverts commit e069730f8e087f27e0c3076d135da295a4de79fd, reversing
    changes made to a9e17d8f424af547f1c4d28635691ce900245732.

--- src/app/data_page/components/data_container/DataContainer.tsx
+++ src/app/data_page/components/data_container/DataContainer.tsx
@@ -1,169 +1,79 @@
 "use client";
-import React, { useState } from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import styles from "./dataContainer.module.css";
+import { DiskDto, BandDto, RegionDto } from '@api/dto';
 
-// Tipos de datos
+/* Data de la documentacion de datagrid */
+/* https://mui.com/x/react-data-grid/ */
+const columns: GridColDef<(typeof rows)[number]>[] = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'Disk',
+      headerName: 'Disk',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'Band',
+      headerName: 'Band',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'Data',
+      headerName: 'Data',
+      width: 110,
+      editable: true,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, Disk: 'Snow', Band: 'Jon', Data: 14 },
+    { id: 2, Disk: 'Lannister', Band: 'Cersei', Data: 31 },
+    { id: 3, Disk: 'Lannister', Band: 'Jaime', Data: 31 },
+    { id: 4, Disk: 'Stark', Band: 'Arya', Data: 11 },
+    { id: 5, Disk: 'Targaryen', Band: 'Daenerys', Data: null },
+    { id: 6, Disk: 'Melisandre', Band: null, Data: 150 },
+    { id: 7, Disk: 'Clifford', Band: 'Ferrara', Data: 44 },
+    { id: 8, Disk: 'Frances', Band: 'Rossini', Data: 36 },
+    { id: 9, Disk: 'Roxie', Band: 'Harvey', Data: 65 },
+  ];
+
 interface DataItem {
-  name: string;
-  creationDate: string;
-  file: string;
-  isViewable: boolean;
+    id: number;
+    Disk: string;
+    Band: string;
+    Data: number | null; // Ajusta el tipo de datos según corresponda
+}
+interface DataContainerProps {
+    dataProps: DataItem[];
 }
 
-interface Molecule {
-  name: string;
-  data: DataItem[];
-}
-
-interface Band {
-  name: string;
-  molecules: Molecule[];
-}
-
-interface Disk {
-  id: string;
-  disk: string;
-  bands: Band[];
-}
-
-// Datos de ejemplo ampliados
-const dummyData: Disk[] = [
-  {
-    id: '1',
-    disk: 'Disk 1',
-    bands: [
-      {
-        name: 'Band 6',
-        molecules: [
-          {
-            name: 'Continuum',
-            data: [
-              { name: 'Measurement Set', creationDate: '2024-04-16', file: 'measurement_set.zip', isViewable: false },
-              { name: 'Map', creationDate: '2024-04-17', file: 'map.fits', isViewable: true },
-            ],
-          },
-          {
-            name: 'Molecule 1',
-            data: [
-              { name: 'Measurement Set', creationDate: '2024-04-16', file: 'measurement_set.zip', isViewable: false },
-              { name: 'Cubo de Canales', creationDate: '2024-04-17', file: 'cubo_de_canales.zip', isViewable: false },
-              { name: 'Momento 0', creationDate: '2024-04-18', file: 'momento_0.fits', isViewable: true },
-              { name: 'Momento 1', creationDate: '2024-04-19', file: 'momento_1.fits', isViewable: true },
-            ],
-          },
-          {
-            name: 'Molecule 2',
-            data: [
-              { name: 'Measurement Set', creationDate: '2024-04-16', file: 'measurement_set.zip', isViewable: false },
-              { name: 'Cubo de Canales', creationDate: '2024-04-17', file: 'cubo_de_canales.zip', isViewable: false },
-              { name: 'Momento 0', creationDate: '2024-04-18', file: 'momento_0.fits', isViewable: true },
-              { name: 'Momento 1', creationDate: '2024-04-19', file: 'momento_1.fits', isViewable: true },
-            ],
-          },
-          {
-            name: 'Molecule N',
-            data: [
-              { name: 'Measurement Set', creationDate: '2024-04-16', file: 'measurement_set.zip', isViewable: false },
-              { name: 'Cubo de Canales', creationDate: '2024-04-17', file: 'cubo_de_canales.zip', isViewable: false },
-              { name: 'Momento 0', creationDate: '2024-04-18', file: 'momento_0.fits', isViewable: true },
-              { name: 'Momento 1', creationDate: '2024-04-19', file: 'momento_1.fits', isViewable: true },
-            ],
-          },
-        ],
-      },
-      {
-        name: 'Band 7',
-        molecules: [
-          {
-            name: 'Continuum',
-            data: [
-              { name: 'Measurement Set', creationDate: '2024-04-16', file: 'measurement_set.zip', isViewable: false },
-              { name: 'Map', creationDate: '2024-04-17', file: 'map.fits', isViewable: true },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: '2',
-    disk: 'Disk N',
-    bands: [
-      {
-        name: 'Band 1',
-        molecules: [
-          {
-            name: 'Continuum',
-            data: [
-              { name: 'Measurement Set', creationDate: '2024-04-16', file: 'measurement_set.zip', isViewable: false },
-              { name: 'Map', creationDate: '2024-04-17', file: 'map.fits', isViewable: true },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-];
-
-const DataContainer: React.FC = () => {
-  const [selectedBands, setSelectedBands] = useState<{ [key: string]: string | null }>({});
-  const [selectedCategories, setSelectedCategories] = useState<{ [key: string]: string | null }>({});
-
-  const handleBandClick = (diskId: string, bandName: string) => {
-    setSelectedBands(prev => ({
-      ...prev,
-      [diskId]: prev[diskId] === bandName ? null : bandName
-    }));
-    setSelectedCategories(prev => ({
-      ...prev,
-      [diskId]: null
-    }));
-  };
-
-  const handleCategoryClick = (diskId: string, categoryName: string) => {
-    setSelectedCategories(prev => ({
-      ...prev,
-      [diskId]: prev[diskId] === categoryName ? null : categoryName
-    }));
-  };
-
-  return (
-    <div className={styles.tableContainer}>
-      {dummyData.map((disk) => (
-        <div className={styles.tableExt} key={disk.id}>
-          <div className={styles.tableInt}>
-            <div className={styles.diskColumn}>
-              <div className={styles.eachDisk}>{disk.disk}</div>
+const DataContainer: React.FC<DataContainerProps> = ({ dataProps }) => {
+    console.log(dataProps)
+    return(
+        <div className={styles.DataContainerExt}>
+            <div className={styles.DataContainerInt}>
+                <Box sx={{ height: 400, width: '100%' }}>
+                    <DataGrid
+                        rows={dataProps}
+                        columns={columns}
+                        initialState={{pagination: {
+                                paginationModel: {
+                                    pageSize: 5,
+                                    },
+                                },
+                        }}
+                        pageSizeOptions={[5]}
+                        checkboxSelection
+                        disableRowSelectionOnClick
+                        />
+                </Box>
             </div>
-            <div className={styles.bandColumn}>
-              {disk.bands.map((band) => (
-                <div key={band.name} onClick={() => handleBandClick(disk.id, band.name)} className={styles.eachBand}>
-                  {band.name}
-                </div>
-              ))}
-            </div>
-            <div className={styles.spectroscopyColumn}>
-                {selectedBands[disk.id] && disk.bands
-                  .find(b => b.name === selectedBands[disk.id])?.molecules
-                  .map((molecule) => (
-                      <div onClick={() => handleCategoryClick(disk.id, molecule.name)} className={styles.eachMolecule}>
-                        {molecule.name}
-                      </div>
-                ))}
-            </div>
-            <div className={styles.dataColumn}>
-                {selectedCategories[disk.id] && disk.bands
-                  .find(b => b.name === selectedBands[disk.id])?.molecules
-                  .find(m => m.name === selectedCategories[disk.id])?.data
-                  .map((dataItem) => (
-                      <div>{dataItem.name}</div>
-                ))}
-            </div>
-          </div>
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default DataContainer;
