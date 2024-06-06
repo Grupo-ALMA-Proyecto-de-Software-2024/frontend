439646f2543bca588820a057c04fc5d29a87da9b
Pablo <pablo.skewes@ug.uchile.cl>
2024-06-06 18:21:46 -0400

    remove parent context from dto's

--- src/api/dto.ts
+++ src/api/dto.ts
@@ -9,30 +9,20 @@ export interface DataDto {
     creationDate: string;
     file: string;
     isViewable: boolean;
-    moleculeName: string;  // Parent context
-    bandName: string;      // Parent context
-    diskName: string;      // Parent context
-    regionName: string;    // Parent context
 }
 
 export interface MoleculeDto {
     name: string;
-    bandName: string;      // Parent context
-    diskName: string;      // Parent context
-    regionName: string;    // Parent context
     data: DataDto[];
 }
 
 export interface BandDto {
     name: string;
-    diskName: string;      // Parent context
-    regionName: string;    // Parent context
     molecules: MoleculeDto[];
 }
 
 export interface DiskDto {
     name: string;
-    regionName: string;    // Parent context
     bands: BandDto[];
 }
 
