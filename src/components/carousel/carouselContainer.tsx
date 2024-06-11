01d08fc8fcf8abf48c5e18cdcee9d810aa4fa047
bverab <bastian.vera@ug.uchile.cl>
2024-06-11 14:20:47 -0400

    update: new loading panel for carousel

--- src/components/carousel/carouselContainer.tsx
+++ src/components/carousel/carouselContainer.tsx
@@ -2,16 +2,9 @@
 import React from 'react';
 import Carousel from '@components/carousel/Carousel';
 import useCarouselImages from './useCarouselImages';
-import styles from "./carousel.module.css";
 
 const CarouselContainer: React.FC = () => {
     const { carouselImages, isLoading, error } = useCarouselImages();
-
-    if (isLoading) {
-        return <div className={styles.divCarga}>
-                <img src="..\imagen_de_carga.gif" alt="Cargando..."/>
-                </div>
-    }
 
     if (error && carouselImages.length === 0) {
         return <div>Error: {error}</div>;
@@ -19,7 +12,7 @@ const CarouselContainer: React.FC = () => {
 
     return (
         <div>
-            <Carousel images={carouselImages} />
+            <Carousel images={carouselImages} isLoading={isLoading} />
         </div>
     );
 }
