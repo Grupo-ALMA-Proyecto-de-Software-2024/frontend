c28b061dcc9045dbb9cc55c5ad2bc71ad3da6dce
bverab <bastian.vera@ug.uchile.cl>
2024-06-28 13:24:07 -0400

    More stylish

--- src/app/data_page/components/data_container/DataContainer.tsx
+++ src/app/data_page/components/data_container/DataContainer.tsx
@@ -75,6 +75,25 @@ const DataContainer: React.FC<{ data: DiskDto[] }> = ({ data }) => {
     setSelectedItems(newSelectedItems);
   };
 
+  useEffect(() => {
+    const flatData: FlattenedDataItem[] = data.flatMap(disk =>
+      disk.bands.flatMap(band =>
+        band.molecules.flatMap(molecule =>
+          molecule.data.map(dataItem => ({
+            ...dataItem,
+            disk: disk.name,
+            band: band.name,
+            molecule: molecule.name,
+          }))
+        )
+      )
+    );
+
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    setPaginatedItems(flatData.slice(start, end));
+  }, [currentPage, data, selectedItems]);
+
   const totalPages = Math.ceil(
     data.reduce((sum, disk) => sum + disk.bands.reduce((sumBand, band) => sumBand + band.molecules.reduce((sumMol, molecule) => sumMol + molecule.data.length, 0), 0), 0) / itemsPerPage
   );
@@ -84,7 +103,13 @@ const DataContainer: React.FC<{ data: DiskDto[] }> = ({ data }) => {
       <table className={styles.table}>
         <TableHeader
           handleSelectAll={handleSelectAll}
-          isSelectedAll={selectedItems.size === paginatedItems.length}
+          isSelectedAll={selectedItems.size === data.flatMap(disk =>
+            disk.bands.flatMap(band =>
+              band.molecules.flatMap(molecule =>
+                molecule.data.map(dataItem => `${disk.name}-${band.name}-${molecule.name}-${dataItem.name}`)
+              )
+            )
+          ).length}
         />
         <TableRow
           data={paginatedItems}
--- src/app/data_page/components/data_container/TableHeader.tsx
+++ src/app/data_page/components/data_container/TableHeader.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { TableHead, TableRow, TableCell, Checkbox } from '@mui/material';
+import { TableCell, TableHead, TableRow, Checkbox, Tooltip, Zoom } from '@mui/material';
 import styles from './dataContainer.module.css';
 
+/**
+ * Props for the TableHeader component.
+ */
 interface TableHeaderProps {
   handleSelectAll: () => void;
   isSelectedAll: boolean;
 }
 
+/**
+ * TableHeader component to render the table headers.
+ * @param {TableHeaderProps} props - The props for the component.
+ */
 const TableHeader: React.FC<TableHeaderProps> = ({ handleSelectAll, isSelectedAll }) => {
   return (
     <TableHead>
@@ -17,7 +24,13 @@ const TableHeader: React.FC<TableHeaderProps> = ({ handleSelectAll, isSelectedAl
         <TableCell>
           <div className={styles.checkboxHeader}>
             Data Item
-            <Checkbox onChange={handleSelectAll} checked={isSelectedAll} />
+            <Tooltip TransitionComponent={Zoom} title="Select All" arrow>
+              <Checkbox
+                onChange={handleSelectAll}
+                checked={isSelectedAll}
+                className={styles.checkbox}
+              />
+            </Tooltip>
           </div>
         </TableCell>
       </TableRow>
